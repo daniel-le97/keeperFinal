0aee623efa9cc8c46753553880a5e99a949640ca
daniel <dnaiel97.le@gmail.com>
2022-12-12 10:19:19 -0700

    1

--- keeperFinal.client/vite.config.js
+++ keeperFinal.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'wwwroot',
+    outDir: '../keeperFinal/wwwroot',
     sourcemap: false
   },
   server: {
